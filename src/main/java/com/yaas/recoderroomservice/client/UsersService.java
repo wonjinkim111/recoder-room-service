33c63a2bcd500b37857bf3bc88df369645e153b7
wonjinkim111 <diffrentstar@naver.com>
2025-04-20 21:29:34 +0900

    250420 Modified UsersService.java -> FeignClient

--- src/main/java/com/yaas/recoderroomservice/client/UsersService.java
+++ src/main/java/com/yaas/recoderroomservice/client/UsersService.java
@@ -6,7 +6,7 @@
 	import org.springframework.web.bind.annotation.RequestParam;
 	
 	@FeignClient(
-	        url = "http://localhost:10000",
+	        //url = "http://localhost:10000",
 	        name = "users-service"
 	)
 	public interface UsersService {
