d377bd70a7352ceb7d50ea7e2f574d33585ae80c
wonjinkim111 <diffrentstar@naver.com>
2025-06-25 20:47:18 +0900

    250625_Modified UsersService

--- src/main/java/com/yaas/recoderroomservice/client/UsersService.java
+++ src/main/java/com/yaas/recoderroomservice/client/UsersService.java
@@ -5,7 +5,7 @@
 	import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.RequestParam;
 	
-	@FeignClient(url = "http://users-service:10000", name = "users-service")
+	@FeignClient(url = "http://localhost:10000", name = "users-service")
 	public interface UsersService {
 	    @GetMapping({"/users/mentor/nickname"})
 	    UmentorNicknameModel getMentorNickname(@RequestParam long paramLong);
